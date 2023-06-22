c79948aa7cfa0dc266c8744d0052edbef58d269b
adenugbamichael <adenugbamicke@gmail.com>
2023-06-22 17:41:47 +0100

    register

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -6,6 +6,7 @@ import FormContainer from "../components/FormContainer"
 import { useLoginMutation } from "../slices/usersApiSlice"
 import { setCredentials } from "../slices/authSlice"
 import { toast } from "react-toastify"
+import Loader from "../components/Loader"
 
 const LoginScreen = () => {
   const [email, setEmail] = useState("")
@@ -63,7 +64,7 @@ const LoginScreen = () => {
           Sign In
         </Button>
 
-        {isLoading && <h2>Loading...</h2>}
+        {isLoading && <Loader />}
 
         <Row className='py-3'>
           <Col>
