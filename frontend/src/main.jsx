e63329b3e18d7275953d0c542d24ac98e8e82847
adenugbamichael <adenugbamicke@gmail.com>
2023-06-22 16:21:29 +0100

    Integrate redux/toolkit | Login authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,9 +9,11 @@ import {
 import App from "./App.jsx"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.css"
+import { Provider } from "react-redux"
 import HomeScreen from "./screens/HomeScreen.jsx"
 import LoginScreen from "./screens/LoginScreen.jsx"
 import RegisterScreen from "./screens/RegisterScreen.jsx"
+import store from "./store.js"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,7 +26,9 @@ const router = createBrowserRouter(
 )
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 )
