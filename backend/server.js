8c5a64064f4a15d15133b7ff33dc4cb8376118a4
adenugbamichael <adenugbamicke@gmail.com>
2023-07-14 09:17:26 +0100

    fix package

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,39 @@
-import dotenv from "dotenv"
-import userRoutes from "./routes/userRoutes.js"
+import path from "path"
 import express from "express"
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import dotenv from "dotenv"
+dotenv.config()
 import connectDB from "./config/db.js"
 import cookieParser from "cookie-parser"
-dotenv.config()
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import userRoutes from "./routes/userRoutes.js"
+
 const port = process.env.PORT || 5000
 
 connectDB()
 
 const app = express()
+
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cookieParser())
 
-app.use(express.json())
-
 app.use("/api/users", userRoutes)
 
-app.get("/", (req, res) => res.send("Server is ready"))
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve()
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....")
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () => console.log(`Server stared at port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`))
